1d91da3e430f535f5f0f1f3fe2d91ec288bf7ecf
Liam McLean <liam.mclean@student.uq.edu.au>
2026-05-20 13:55:27 +1000

    Increase Camera scan speed

--- camera/ImageDetection.py
+++ camera/ImageDetection.py
@@ -3,11 +3,9 @@ import numpy as np
 import cv2
 from cvzone.HandTrackingModule import HandDetector
 
-url = "http://192.168.20.74/capture"
+url = "http://10.160.24.114/capture"
 
-detector = HandDetector(detectionCon=0.8, maxHands=1)
-
-while True:
+def getFingerData():
     try:
         imgResp = urllib.request.urlopen(url)
         imgNp = np.array(bytearray(imgResp.read()), dtype=np.uint8)
@@ -19,14 +17,22 @@ while True:
         if hands:
             hand = hands[0]
             fingers = detector.fingersUp(hand)
-            print(fingers)
+            #print(fingers)
 
         cv2.putText(img, f"Fingers: {sum(fingers)}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        cv2.imwrite("fingersMarked.jpg", img)
         cv2.imshow("Hand Detection", img)
 
-        if cv2.waitKey(1) == 27:
-            break
     except Exception as e:
         print("Error: ", e)
+    return fingers
 
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    detector = HandDetector(detectionCon=0.8, maxHands=1)
+    while True:
+        getFingerData()
+        if cv2.waitKey(1) == 27:
+            break
+    cv2.destroyAllWindows()
+
+
